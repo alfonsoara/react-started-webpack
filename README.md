f688f6dd498f07067ba36b5650e63722cc580aa0
Alfonso Aranda <alfonsoara@iMac-de-Alfonso.local>
2018-02-21 14:03:27 -0600

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# react-started-webpack
-Base para empezar proyecto con Reactjs + Webpack
+# ReactJS - Webpack - Yarn
+
+### Installation
+
+Dillinger requires [Node.js](https://nodejs.org/) v4+ to run.
+
+Install the dependencies and devDependencies and start the server.
+
+```sh
+$ cd react-started-webpack
+$ brew install yarn | yarn install  
+$ npm run start
+```
+For build...
+```sh
+$ npm run build
+```
+
+For production environments...
+
+```sh
+$ npm run production
+```
---
+++ src/server/index.js
@@ -0,0 +1,42 @@
+// Dependencies
+import express from 'express';
+import webpack from 'webpack';
+import path from 'path';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import open from 'open';
+
+// Webpack Configuration
+import webpackConfig from '../../webpack.config.babel';
+
+// Server Port
+const port = 3000;
+
+// Environment
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+// Express app
+const app = express();
+
+// Public
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Webpack Compiler
+const webpackCompiler = webpack(webpackConfig);
+
+if (isDevelopment) {
+  app.use(webpackDevMiddleware(webpackCompiler));
+  app.use(webpackHotMiddleware(webpackCompiler));
+}
+
+// Sending all the traffic to React
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// Listen port 3000
+app.listen(port, err => {
+  if (!err) {
+    open(`http://localhost:${port}`);
+  }
+});
